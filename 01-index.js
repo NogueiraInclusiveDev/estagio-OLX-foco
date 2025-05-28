78c2735792da7011549b4268a962b107d80dfda9
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2025-05-28 00:24:30 -0300

    dois

---
+++ 01-index.js
@@ -0,0 +1,24 @@
+function soma(num1,num2){
+  return num1 + num2
+}
+
+console.log(soma(10,15));
+
+
+let aluno = "Romulo";
+aluno = {
+  nome:"Romulo Nogueira",
+  idade: 40
+};
+console.log(aluno);
+
+var valorDoServico = 20.00;
+valorDoServico = [13.99, 34.87, 29.99];
+
+var nomeusuario = "João";
+var nomeUsuario = "Tiago";
+
+console.log("nomeusuario = " + nomeusuario);
+
+
+console.log("nomeUsuario = " + nomeUsuario);
